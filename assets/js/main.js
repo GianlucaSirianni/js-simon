37567991f26367648f74f6b8bdc39cab8aa3b7db
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2022-11-03 17:41:19 +0100

    manca capire se i due array sono uguali + css

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,28 +29,63 @@
 //     console.log(numArray);
 // }
 
+// `<p>${numArray[i]}</p>`;
 
+function start() {
+    setTimeout(play, 3000);
+    document.getElementById('numContainer').innerHTML = 'Prepara la tua memoria, tra 5 secondi questi numeri spariranno, prova a ricordartene il più possibile';
+    console.log('preparati');
 
-function play(){
-    console.log('lets play');
-    numArray = [];
-    while(numArray.length < 5){
-        console.log('siamo dentro al while')
-        let num = Math.floor(Math.random() * 100) + 1;
-        if (numArray.includes(num) !== true){
-            numArray.push(num);
+    function play() {
+        setTimeout(hideNum, 5000);
+        setTimeout(askNum, 6000)
+        document.getElementById('numContainer').innerHTML = ' ';
+        console.log('lets play');
+        numArray = [];
+        guess = [];
+        while (numArray.length < 5) {
+            console.log('siamo dentro al while')
+            let num = Math.floor(Math.random() * 100) + 1;
+            if (numArray.includes(num) !== true) {
+                numArray.push(num);
+            }
         }
-    }
-    console.log(numArray);
+        console.log(numArray);
 
-    function createDiv() {
-        const div = document.createElement("div");
-        return div;
-      }
+        function createDiv() {
+            const div = document.createElement("div");
+            return div;
+        }
 
-    for (i = 0; i< numArray.length; i++){
-        let currentElement = createDiv();
-        currentElement.classList.add('measure');
-        currentElement.innerHTML += `<p>${numArray[i]}</p>`;
+
+        for (i = 0; i < numArray.length; i++) {
+            document.getElementById('numContainer').innerHTML += numArray[i] + ' ';
+        }
+
+        function hideNum() {
+            document.getElementById('numContainer').innerHTML = ' ';
+        }
+
+        function askNum() {
+            for (i = 1; i < 6; i++) {
+                var input = parseInt(prompt("Inserisci il " + (i) + "° numero"))
+                guess.push(input);
+                console.log(guess);
+            }
+
+            for (i=0; i<numArray.length; i++){
+                if ((numArray.includes(input) == true) && (numArray.length == guess.length)){
+                    document.getElementById('numContainer').innerHTML = 'Bravissim*, li hai indovinati tutti';
+                } else {
+                    document.getElementById('numContainer').innerHTML = 'ahi ahi ahi, qualcosa non va';
+                }
+            }
+        }
+
+
     }
+
+
+
 }
+
